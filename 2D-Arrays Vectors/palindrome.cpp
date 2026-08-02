19904833756e86e98ec53679e8c687624db486b9
Aimal Khan <aiimallkhann@gmail.com>
2026-08-02 12:27:23 +0500

    Add new Palindrome Number solution with edge cases.

--- 2D-Arrays Vectors/palindrome.cpp
+++ 2D-Arrays Vectors/palindrome.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main() {
-    int x = 12;
-    int res = x;
-    int mod;
-    int rev = 0;
-    int result = x;
-
-    while (result != 0) {
-        mod = result % 10;
-        rev = rev * 10 + mod;
-        result = result / 10;
-    }
-
-    if (rev == x) {
-        cout << x << " is a palindrome." << endl;
+    int x = 10;
+    if (x < 0) {
+        cout << false;
+        return false;
     } else {
-        cout << x << " is not a plaindrome." << endl;
+        int res = x;
+        int mod;
+        int rev = 0;
+        int result = x;
+
+        while (result != 0) {
+            mod = result % 10;
+            rev = rev * 10 + mod;
+            result = result / 10;
+        } if (rev == x){cout << true; return true;} else{cout << false; return false;}
     }
 
     return 0;
